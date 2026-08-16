10bfed357c2f4cc3ac283293ff48e588453b30fd
JForster89 <james.forster@iceland.co.uk>
2026-08-16 22:23:45 +0100

    Shopping list 16 Aug 22:23

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Cache the list so it opens in a supermarket dead spot.
-var CACHE = 'shopping-20260816221730';
+var CACHE = 'shopping-20260816222342';
 var ASSETS = ['./', './index.html', './cook.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', function (e) {
