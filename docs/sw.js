c90cac040453c3af2c31fc85b52c03175e2f7b9a
JForster89 <james.forster@iceland.co.uk>
2026-08-16 23:58:36 +0100

    Shopping list 16 Aug 23:58

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Cache the list so it opens in a supermarket dead spot.
-var CACHE = 'shopping-20260816233939';
-var ASSETS = ['./', './index.html', './cook.html', './manifest.json', './icon.svg', './img/088159ecccb86614.jpg', './img/247051050ac9062f.jpg', './img/3a414dba654d9b9f.jpg', './img/48b42d94e75e11cb.jpg', './img/4db0aaa59e637aab.jpg', './img/5bd7012772b41b2c.jpg', './img/6166a0c927e30be2.jpg', './img/6e1c8da292e48b8e.jpg', './img/73c6f4f6aec4038b.jpg', './img/7ed1045b4bff6300.jpg', './img/c4655151fd4569d5.jpg', './img/d98a4a73e38cf54f.jpg'];
+var CACHE = 'shopping-20260816235833';
+var ASSETS = ['./', './index.html', './cook.html', './manifest.json', './icon.svg', './img/088159ecccb86614.jpg', './img/0f7025d06cc45a1b.jpg', './img/1d33a6fcae905c37.jpg', './img/247051050ac9062f.jpg', './img/302609d605f74fcd.jpg', './img/3a414dba654d9b9f.jpg', './img/48b42d94e75e11cb.jpg', './img/4db0aaa59e637aab.jpg', './img/5bd7012772b41b2c.jpg', './img/5de7d5a70615dd0b.jpg', './img/6166a0c927e30be2.jpg', './img/672f1c51625dbdf0.jpg', './img/6e1c8da292e48b8e.jpg', './img/73c6f4f6aec4038b.jpg', './img/7a22a8c52dd559f4.jpg', './img/7ed1045b4bff6300.jpg', './img/b7694744c13a520b.jpg', './img/c4655151fd4569d5.jpg', './img/d342c74ed75beee5.jpg', './img/d98a4a73e38cf54f.jpg', './img/dd31747212233743.jpg', './img/e5940e59d9dab8a1.jpg'];
 
 self.addEventListener('install', function (e) {
   self.skipWaiting();
