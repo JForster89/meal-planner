d4d6fc2adf78c13ed0d88efce3ac14e5d4d6038b
JForster89 <james.forster@iceland.co.uk>
2026-08-16 23:37:48 +0100

    Shopping list 16 Aug 23:37

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Cache the list so it opens in a supermarket dead spot.
-var CACHE = 'shopping-20260816224514';
-var ASSETS = ['./', './index.html', './cook.html', './manifest.json', './icon.svg'];
+var CACHE = 'shopping-20260816233744';
+var ASSETS = ['./', './index.html', './cook.html', './manifest.json', './icon.svg', './img/088159ecccb86614.jpg', './img/247051050ac9062f.jpg', './img/3a414dba654d9b9f.jpg', './img/48b42d94e75e11cb.jpg', './img/4db0aaa59e637aab.jpg', './img/5bd7012772b41b2c.jpg', './img/6166a0c927e30be2.jpg', './img/6e1c8da292e48b8e.jpg', './img/73c6f4f6aec4038b.jpg', './img/7ed1045b4bff6300.jpg', './img/c4655151fd4569d5.jpg', './img/d98a4a73e38cf54f.jpg'];
 
 self.addEventListener('install', function (e) {
   self.skipWaiting();
